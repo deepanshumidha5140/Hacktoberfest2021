be5d7ef805523d319ae4b6a150c03f35ff86d315
ALBY SABU <41825993+albysabu9@users.noreply.github.com>
2021-10-29 11:39:59 +0530

    Update starpattern.cpp

--- C++/starpattern.cpp
+++ C++/starpattern.cpp
@@ -1,1 +1,21 @@
-#test
+//this program is for stat pattern printing
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows; ++i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
